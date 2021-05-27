dde18a17ec94cebc709f24ef08e64cef7c60f7f3
Molly Hanlon <hanlonmt@gmail.com>
2021-05-27 16:05:08 -0400

    Update Publication_formatter.R

--- Publication_formatter.R
+++ Publication_formatter.R
@@ -5,3 +5,10 @@ library(bib2academic)
 my_bibfile <- "Citations_2021_05_27.bib"
 
 bib2acad(bibfile = my_bibfile)
+
+
+# to add publications to this, get citations from scopus or manually add the bib info to the "Citations_2021_05_27.bib file"
+# then run this code to generate bib and md files
+# then copy the md file that is new from the folder to the folder
+# content/publication
+# anything new needs to be in content
